80b4dd9672452d0a06e905e2cc4830fc1fc5ae7e
preethi-09 <preethicharles09@gmail.com>
2022-09-20 18:19:55 +0530

    template

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function create()
+    {
+        return view('frontend/include/sample');
+    }
+     public function click()
+    {
+        return view('frontend/include/sample1');
+    }
+
+}
